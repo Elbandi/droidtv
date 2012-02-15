10f500e265b7c08ea0dd265c0385ffdb0b01f0d5
chrulri <chrulri@gmail.com>
2012-02-15 22:40:03 +0100

    new w_scan (version 20110329) compiled using android ndk r7b

---
+++ res-src/README.md
@@ -0,0 +1,17 @@
+HOWTO BUILD W_SCAN
+==================
+
+ENTER THIS DIRECTORY..
+
+## DOWNLOAD SOURCES ##
+```linux shell
+$ wget http://wirbel.htpc-forum.de/w_scan/w_scan-20110329.tar.bz2
+```
+
+## BUILD W_SCAN ##
+```linux shell
+$ tar xf w_scan-20110329.tar.bz2
+$ ./build_wscan.sh
+```
+the compiled w_scan binary will then be copied to /res/raw/
+
---
+++ res-src/build_wscan.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# droidtv/res-srv/build_wscan.sh
+# Compiles w_scan for Android
+# Make sure you have ANDROID_NDK defined in .bashrc or .bash_profile
+
+API=8
+SRC_DIR="`pwd`/w_scan-20110329"
+TARGET_FILE="`pwd`/../res/raw/wscan_20110329.bin"
+INSTALL_DIR="`pwd`/../bin"
+INCLUDE_DIR="`pwd`/../jni/include"
+
+cd $SRC_DIR
+
+export PATH="$ANDROID_NDK/toolchains/arm-linux-androideabi-4.4.3/prebuilt/linux-x86/bin/:$PATH"
+export SYS_ROOT="$ANDROID_NDK/platforms/android-$API/arch-arm/"
+export CC="arm-linux-androideabi-gcc --sysroot=$SYS_ROOT"
+export LD="arm-linux-androideabi-ld"
+export AR="arm-linux-androideabi-ar"
+export RANLIB="arm-linux-androideabi-ranlib"
+export STRIP="arm-linux-androideabi-strip"
+
+mkdir -p $INSTALL_DIR
+./configure --host=arm-eabi --prefix=$INSTALL_DIR LIBS="-lc -lgcc" CFLAGS="-I$INCLUDE_DIR"
+
+make
+make install
+
+cp $INSTALL_DIR/bin/w_scan $TARGET_FILE
+
+exit 0
+
