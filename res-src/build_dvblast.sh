c106aea28297b46297fa6ef6f55b8f4d7f6dea40
chrulri <chrulri@gmail.com>
2012-03-21 04:18:57 +0100

    build instruction, #4

--- res-src/build_dvblast.sh
+++ res-src/build_dvblast.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 # droidtv/res-srv/build_mumudvb.sh
-# Compiles mumudvb for Android
+# Compiles DVBlast for Android
 # Make sure you have ANDROID_NDK defined in .bashrc or .bash_profile
 
 API=8
-SRC_DIR="`pwd`/mumudvb"
-TARGET_FILE="`pwd`/../res/raw/mumudvb_1_7.bin"
+SRC_DIR_DVBLAST="`pwd`/dvblast-2.1.0"
+SRC_DIR_BITSTREAM="`pwd`/bitstream-1.0"
+TARGET_FILE="`pwd`/../res/raw/dvblast_2_1_0.bin"
+TARGETCTL_FILE="`pwd`/../res/raw/dvblastctl_2_1_0.bin"
 INSTALL_DIR="`pwd`/../bin"
 INCLUDE_DIR="`pwd`/include"
 
-cd $SRC_DIR
 
 export PATH="$ANDROID_NDK/toolchains/arm-linux-androideabi-4.4.3/prebuilt/linux-x86/bin/:$PATH"
 export SYS_ROOT="$ANDROID_NDK/platforms/android-$API/arch-arm/"
@@ -18,20 +19,21 @@ export LD="arm-linux-androideabi-ld"
 export AR="arm-linux-androideabi-ar"
 export RANLIB="arm-linux-androideabi-ranlib"
 export STRIP="arm-linux-androideabi-strip"
+export CFLAGS="-I$INSTALL_DIR/include -I$INCLUDE_DIR"
+export LDLIBS="-lc -lgcc"
+export PREFIX="$INSTALL_DIR"
 
 mkdir -p $INSTALL_DIR
-./configure --host=arm-eabi --prefix=$INSTALL_DIR LIBS="-lc -lgcc" CFLAGS="-I$INCLUDE_DIR"
 
-echo "#undef malloc" >> src/config.h
-echo "#undef realloc" >> src/config.h
-echo "#define HAVE_LIBPTHREAD 1" >> src/config.h
-echo "#include <pthread.h>" >> src/config.h
-echo "#define pthread_timedjoin_np(a,b,c) pthread_join(a,b)" >> src/config.h
+cd $SRC_DIR_BITSTREAM
+make install
 
+cd $SRC_DIR_DVBLAST
 make
 make install
 
-cp $INSTALL_DIR/bin/mumudvb $TARGET_FILE
+cp $INSTALL_DIR/bin/dvblast $TARGET_FILE
+cp $INSTALL_DIR/bin/dvblastctl $TARGETCTL_FILE
 
 exit 0
 
