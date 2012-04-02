f5aa4d102b40d18506b92c98ea7cd544dd0eb53a
chrulri <chrulri@gmail.com>
2012-04-02 20:46:18 +0200

    cleanup, #6

--- src/com/chrulri/droidtv/PreferencesActivity.java
+++ src/com/chrulri/droidtv/PreferencesActivity.java
@@ -18,14 +18,10 @@
 
 package com.chrulri.droidtv;
 
-import static com.chrulri.droidtv.Utils.Prefs.KEY_CHANNELCONFIGS;
 import static com.chrulri.droidtv.Utils.Prefs.KEY_SCANCHANNELS;
-
-import java.io.File;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
@@ -47,22 +43,5 @@ public class PreferencesActivity extends PreferenceActivity {
                 return true;
             }
         });
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        // read channel configs from filesystem
-        ListPreference channelConfigs = (ListPreference) findPreference(KEY_CHANNELCONFIGS);
-        File configsDir = Utils.getConfigsDir(this);
-        String[] configFiles = configsDir.list();
-        String[] configs = new String[configFiles.length];
-        for (int i = 0; i < configFiles.length; i++) {
-            String fileName = configFiles[i];
-            int pos = fileName.lastIndexOf(".conf");
-            configs[i] = pos == -1 ? fileName : fileName.substring(0, pos);
-        }
-        channelConfigs.setEntries(configs);
-        channelConfigs.setEntryValues(configFiles);
     }
 }
